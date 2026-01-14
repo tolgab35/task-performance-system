3fa079a9bc01c9d9b3f1fb7edefd80cb17f54b5c
Tolga Boz <tolgaboz350@gmail.com>
2026-01-14 22:33:47 +0300

    feat: add project service for creating projects

---
+++ frontend/src/services/projectService.js
@@ -0,0 +1,8 @@
+import api from './api';
+
+export const projectService = {
+  async createProject(name, description) {
+    const response = await api.post('/projects', { name, description });
+    return response.data;
+  },
+};
